0f3b0c5163312d10a999a9ae56252c6165994731
Oleksand Vdodovych <oleksandr.vdodovych@gmail.com>
2018-03-22 15:43:58 +0200

    Added sign component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SignComponent } from './sign/sign.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule
